22e4695aa5b0ac2f96a7bac3968777fa0e9bc374
Yanis Falaki <yanisfalaki@outlook.com>
2022-07-26 23:59:02 -0400

    changed text, removed unused code

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/reserve")
-def reserve():
-    return render_template("reserve.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
